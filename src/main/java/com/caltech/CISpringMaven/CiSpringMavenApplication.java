9b0d1bebc64545c5a529065c3460401040742d28
student <suvo.ece@gmail.com>
2021-05-12 13:25:57 +0000

    First Commit for CISpring-Maven

---
+++ src/main/java/com/caltech/CISpringMaven/CiSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpringMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/CISpringMaven/CiSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpringMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
